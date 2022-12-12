18290ed78379a496c80b42608740c4f6b4ed2378
AMfaraz <abdulmajeedfarazkalhoro@gmail.com>
2022-12-12 05:21:25 +0500

    2nd Commit

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -9,26 +9,31 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imgurl!,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        title: Text(
-          title!,
-          textAlign: TextAlign.center,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          imgurl!,
+          fit: BoxFit.cover,
         ),
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: Icon(Icons.favorite),
-          onPressed: (){
-            print("hello");
-          },
-        ),
-        trailing:IconButton(
-          icon: Icon(Icons.shopping_cart),
-          onPressed: (){},
+        footer: GridTileBar(
+          title: Text(
+            title!,
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+            onPressed: (){
+              print("hello");
+            },
+          ),
+          trailing:IconButton(
+            icon: Icon(Icons.shopping_cart, ),
+            color: Theme.of(context).accentColor,
+            onPressed: (){},
+          ),
         ),
       ),
     );
